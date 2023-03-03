44403465ebac2580e35c5aaa5d07b6e4c8b548cc
Pedro Marquez <pedro@controllino.com>
2023-03-03 13:34:22 +0100

    Unit tests for arduino neo_pin_mcp356x

--- test/arduino/neo_core/pin/spi_drv/test_neo_pin_mcp356x/mcp356x_mock_spi.cpp
+++ test/arduino/neo_core/pin/spi_drv/test_neo_pin_mcp356x/mcp356x_mock_spi.cpp
@@ -3,6 +3,7 @@
 #include "hw_platform.h"
 #include "mcp356x.h"
  
+extern uint32_t TEST_MCP356X_ADC_DATA;
 static uint8_t cfg2_val = MCP356X_CFG_2_BOOST_X_1 | MCP356X_CFG_2_GAIN_X_2 | MCP356X_CFG_2_AZ_MUX_DIS | MCP356X_CFG_2_AZ_VREF_DIS;
 static uint8_t cfg3_val = MCP356X_CFG_3_CONV_MODE_CONT | MCP356X_CFG_3_DATA_FORMAT_DEF | MCP356X_CFG_3_CRC_COM_DIS | MCP356X_CFG_3_CRC_GAIN_CAL_DIS;
  
@@ -26,6 +27,14 @@ platform_err_code_t mock_spi_transfer(uint8_t* txdata, uint8_t* rxdata, size_t l
             cfg3_val = *(txdata + 1);
         else
             *(rxdata + 1) = cfg3_val;
+    }
+    else if (fcmd_addr == MCP356X_REG_ADC_DATA) {
+        if (rw_cmd == MCP356X_CMD_INC_READ)
+        {
+            *(rxdata + 1) = (TEST_MCP356X_ADC_DATA >> 16) & 0xFF;
+            *(rxdata + 2) = (TEST_MCP356X_ADC_DATA >> 8) & 0xFF;
+            *(rxdata + 3) = TEST_MCP356X_ADC_DATA & 0xFF;
+        }
     }
     *rxdata = MCP356X_VALID_STATUS_MASK;
     return PLATFORM_OK;
--- test/arduino/neo_core/pin/spi_drv/test_neo_pin_mcp356x/test_neo_pin_mcp356x.cpp
+++ test/arduino/neo_core/pin/spi_drv/test_neo_pin_mcp356x/test_neo_pin_mcp356x.cpp
@@ -1,15 +1,78 @@
 #include <unity.h>
 #include "Arduino.h"
  
+#define TEST_MCP356X_ADC_READING_ERROR 100
+uint32_t TEST_MCP356X_ADC_DATA = 0x7FFFFF;
+ 
 void setUp(void)
-{}
+{
+    initVariant();
+    pinMode(NEO_CORE_AI0, INPUT);
+}
  
 void tearDown(void)
 {}
  
+void test_mcp356x_pin_definitions_ok()
+{
+    TEST_ASSERT_EQUAL(MCP356X_CH_CH0, NEO_CORE_AI0->getPin());
+    TEST_ASSERT_EQUAL(ControllinoNeoPin::MCP356X_PIN, NEO_CORE_AI0->getType());
+}
+ 
+void test_mcp356x_pinMode_always_input()
+{
+    TEST_ASSERT_EQUAL(INPUT, NEO_CORE_AI0->getMode());
+    pinMode(NEO_CORE_AI0, OUTPUT);
+    TEST_ASSERT_EQUAL(INPUT, NEO_CORE_AI0->getMode());
+    pinMode(NEO_CORE_AI0, INPUT_PULLUP);
+    TEST_ASSERT_EQUAL(INPUT, NEO_CORE_AI0->getMode());
+    pinMode(NEO_CORE_AI0, INPUT_PULLDOWN);
+    TEST_ASSERT_EQUAL(INPUT, NEO_CORE_AI0->getMode());
+    pinMode(NEO_CORE_AI0, OUTPUT_2MA);
+    TEST_ASSERT_EQUAL(INPUT, NEO_CORE_AI0->getMode());
+    pinMode(NEO_CORE_AI0, OUTPUT_4MA);
+    TEST_ASSERT_EQUAL(INPUT, NEO_CORE_AI0->getMode());
+    pinMode(NEO_CORE_AI0, OUTPUT_8MA);
+    TEST_ASSERT_EQUAL(INPUT, NEO_CORE_AI0->getMode());
+    pinMode(NEO_CORE_AI0, OUTPUT_12MA);
+    TEST_ASSERT_EQUAL(INPUT, NEO_CORE_AI0->getMode());
+}
+ 
+void test_mcp356x_analogRead_ok()
+{
+    int adcArduino = analogRead(NEO_CORE_AI0);
+    int adc_mcp356x;
+    uint8_t dummySgn;
+    uint32_t dummyRes;
+    mcp356x_read_raw_adc(neo_mcp356x, (uint32_t*)&adc_mcp356x, &dummySgn, &dummyRes);
+    TEST_ASSERT_LESS_OR_EQUAL(TEST_MCP356X_ADC_READING_ERROR, abs(adcArduino - adc_mcp356x));
+#ifdef NATIVE_ENV
+    /* Only posible to read back with native env mock */
+    TEST_ASSERT_EQUAL(TEST_MCP356X_ADC_DATA, adcArduino);
+#endif
+}
+ 
+void test_analogWrite_does_nothing()
+{
+    /* Just test if compiles */
+    analogWrite(NEO_CORE_AI0, 0);
+}
+ 
+void test_digitalWrite_digitalRead_does_nothing()
+{
+    digitalWrite(NEO_CORE_AI0, HIGH);
+    int res = digitalRead(NEO_CORE_AI0);
+    TEST_ASSERT_EQUAL(0, res);
+}
+ 
 int runUnityTests(void)
 {
     UNITY_BEGIN();
+    RUN_TEST(test_mcp356x_pin_definitions_ok);
+    RUN_TEST(test_mcp356x_pinMode_always_input);
+    RUN_TEST(test_mcp356x_analogRead_ok);
+    RUN_TEST(test_analogWrite_does_nothing);
+    RUN_TEST(test_digitalWrite_digitalRead_does_nothing);
     UNITY_END();
     return UNITY_END();
 }
