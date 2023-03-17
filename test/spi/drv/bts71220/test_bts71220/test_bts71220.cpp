db85713c8fee2b5fb0f1a8a14358307d9c062b67
Pedro Marquez <pedro@controllino.com>
2023-03-17 15:05:02 +0100

    Better bts71220 unit tests

--- test/spi/drv/bts71220/test_bts71220/test_bts71220.cpp
+++ test/spi/drv/bts71220/test_bts71220/test_bts71220.cpp
@@ -2,7 +2,6 @@
 #include "hw_platform.h"
 #include "bts71220.h"
  
-uint8_t TEST_BTS71220_DAISY_CHAIN_NUMBER = 0; /* Indicate only first BTS71220 will be used in tests*/
 static bts71220_cfg_t cfg;
 static bts71220_t dev;
 static bts71220_err_code_t ret;
@@ -15,7 +14,8 @@ void setUp(void)
  
 void tearDown(void)
 {
-    bts71220_write_reg(&dev, BTS71220_REG_OUT, 0x00, TEST_BTS71220_DAISY_CHAIN_NUMBER);
+    for (size_t i = 0; i < BTS71220_DAISY_CHAIN_SIZE; i++)
+        bts71220_write_reg(&dev, BTS71220_REG_OUT, 0x00, i);
 }
  
 void test_bts71220_set_default_cfg()
@@ -40,68 +40,80 @@ void test_bts71220_init_ok()
  
 void test_bts71220_read_reg_ok()
 {
-    uint8_t rxdata = 0xFF;
-    ret = bts71220_read_reg(&dev, BTS71220_REG_OUT, &rxdata, TEST_BTS71220_DAISY_CHAIN_NUMBER);
-    TEST_ASSERT_EQUAL(PLATFORM_OK, ret);
-    TEST_ASSERT_EQUAL(0x00, (rxdata & (~BTS71220_WRITE_MASK & 0xFF)));
+    for (size_t i = 0; i < BTS71220_DAISY_CHAIN_SIZE; i++) {
+        uint8_t rxdata = 0xFF;
+        ret = bts71220_read_reg(&dev, BTS71220_REG_OUT, &rxdata, i);
+        TEST_ASSERT_EQUAL(PLATFORM_OK, ret);
+        TEST_ASSERT_EQUAL(0x00, (rxdata & (~BTS71220_WRITE_MASK & 0xFF)));
+    }
 }
  
 void test_bts71220_write_reg_ok()
 {
-    uint8_t txdata = 0x03;
-    uint8_t rxdata = 0x00;
-    ret = bts71220_write_reg(&dev, BTS71220_REG_OUT, txdata, TEST_BTS71220_DAISY_CHAIN_NUMBER);
-    TEST_ASSERT_EQUAL(PLATFORM_OK, ret);
-    bts71220_read_reg(&dev, BTS71220_REG_OUT, &rxdata, TEST_BTS71220_DAISY_CHAIN_NUMBER);
-    TEST_ASSERT_EQUAL(txdata, (rxdata & (~BTS71220_WRITE_MASK & 0xFF)));
+    for (size_t i = 0; i < BTS71220_DAISY_CHAIN_SIZE; i++) {
+        uint8_t txdata = 0x03;
+        uint8_t rxdata = 0x00;
+        ret = bts71220_write_reg(&dev, BTS71220_REG_OUT, txdata, i);
+        TEST_ASSERT_EQUAL(PLATFORM_OK, ret);
+        bts71220_read_reg(&dev, BTS71220_REG_OUT, &rxdata, i);
+        TEST_ASSERT_EQUAL(txdata, (rxdata & (~BTS71220_WRITE_MASK & 0xFF)));
+    }
 }
  
 void test_bts71220_try_write_read_only_reg_err()
 {
-    ret = bts71220_write_reg(&dev, BTS71220_REG_WRN_DIAG, 0x00, TEST_BTS71220_DAISY_CHAIN_NUMBER);
+    ret = bts71220_write_reg(&dev, BTS71220_REG_WRN_DIAG, 0x00, 0);
     TEST_ASSERT_EQUAL(PLATFORM_ARGUMENT_ERR, ret);
-    ret = bts71220_write_reg(&dev, BTS71220_REG_STD_DIAG, 0x00, TEST_BTS71220_DAISY_CHAIN_NUMBER);
+    ret = bts71220_write_reg(&dev, BTS71220_REG_STD_DIAG, 0x00, 0);
     TEST_ASSERT_EQUAL(PLATFORM_ARGUMENT_ERR, ret);
-    ret = bts71220_write_reg(&dev, BTS71220_REG_ERR_DIAG, 0x00, TEST_BTS71220_DAISY_CHAIN_NUMBER);
+    ret = bts71220_write_reg(&dev, BTS71220_REG_ERR_DIAG, 0x00, 0);
     TEST_ASSERT_EQUAL(PLATFORM_ARGUMENT_ERR, ret);
-    ret = bts71220_write_reg(&dev, BTS71220_REG_RCS, 0x00, TEST_BTS71220_DAISY_CHAIN_NUMBER);
+    ret = bts71220_write_reg(&dev, BTS71220_REG_RCS, 0x00, 0);
     TEST_ASSERT_EQUAL(PLATFORM_ARGUMENT_ERR, ret);
-    ret = bts71220_write_reg(&dev, BTS71220_REG_SRC, 0x00, TEST_BTS71220_DAISY_CHAIN_NUMBER);
+    ret = bts71220_write_reg(&dev, BTS71220_REG_SRC, 0x00, 0);
     TEST_ASSERT_EQUAL(PLATFORM_ARGUMENT_ERR, ret);
 }
  
 void test_bts71220_read_wrn_diag_ok()
 {
-    uint8_t rxdata;
-    ret = bts71220_read_wrn_diag(&dev, &rxdata, TEST_BTS71220_DAISY_CHAIN_NUMBER);
-    TEST_ASSERT_EQUAL(PLATFORM_OK, ret);
-    TEST_ASSERT_TRUE(rxdata & (BTS71220_ERR_WARN_MASK | BTS71220_REG_CONT_MASK));
+    for (size_t i = 0; i < BTS71220_DAISY_CHAIN_SIZE; i++) {
+        uint8_t rxdata;
+        ret = bts71220_read_wrn_diag(&dev, &rxdata, i);
+        TEST_ASSERT_EQUAL(PLATFORM_OK, ret);
+        TEST_ASSERT_TRUE(rxdata & (BTS71220_ERR_WARN_MASK | BTS71220_REG_CONT_MASK));
+    }
 }
  
 void test_bts71220_read_err_diag_ok()
 {
-    uint8_t rxdata;
-    ret = bts71220_read_err_diag(&dev, &rxdata, TEST_BTS71220_DAISY_CHAIN_NUMBER);
-    TEST_ASSERT_EQUAL(PLATFORM_OK, ret);
-    TEST_ASSERT_TRUE(rxdata & (BTS71220_ERR_WARN_MASK | BTS71220_REG_CONT_MASK));
+    for (size_t i = 0; i < BTS71220_DAISY_CHAIN_SIZE; i++) {
+        uint8_t rxdata;
+        ret = bts71220_read_err_diag(&dev, &rxdata, i);
+        TEST_ASSERT_EQUAL(PLATFORM_OK, ret);
+        TEST_ASSERT_TRUE(rxdata & (BTS71220_ERR_WARN_MASK | BTS71220_REG_CONT_MASK));
+    }
 }
  
 void test_bts71220_set_sense_mux_ok()
 {
-    bts71220_dcr_reg_t dcr_reg;
-    ret = bts71220_set_sense_mux(&dev, BTS71220_DCR_MUX_IS_Z, TEST_BTS71220_DAISY_CHAIN_NUMBER);
-    TEST_ASSERT_EQUAL(PLATFORM_OK, ret);
-    bts71220_read_reg(&dev, BTS71220_REG_DCR, (uint8_t*)&dcr_reg, TEST_BTS71220_DAISY_CHAIN_NUMBER);
-    TEST_ASSERT_EQUAL(BTS71220_DCR_MUX_IS_Z, dcr_reg.mux);
+    for (size_t i = 0; i < BTS71220_DAISY_CHAIN_SIZE; i++) {
+        bts71220_dcr_reg_t dcr_reg;
+        ret = bts71220_set_sense_mux(&dev, BTS71220_DCR_MUX_IS_Z, i);
+        TEST_ASSERT_EQUAL(PLATFORM_OK, ret);
+        bts71220_read_reg(&dev, BTS71220_REG_DCR, (uint8_t*)&dcr_reg, i);
+        TEST_ASSERT_EQUAL(BTS71220_DCR_MUX_IS_Z, dcr_reg.mux);
+    }
 }
-
+ 
 void test_bts71220_mux_sleep_mode_after_init()
 {
-    bts71220_dcr_reg_t dcr_reg;
-    bts71220_read_reg(&dev, BTS71220_REG_DCR, (uint8_t*)&dcr_reg, TEST_BTS71220_DAISY_CHAIN_NUMBER);
-    TEST_ASSERT_EQUAL(BTS71220_DCR_MUX_IS_SLEEP_Z, dcr_reg.mux);
+    for (size_t i = 0; i < BTS71220_DAISY_CHAIN_SIZE; i++) {
+        bts71220_dcr_reg_t dcr_reg;
+        bts71220_read_reg(&dev, BTS71220_REG_DCR, (uint8_t*)&dcr_reg, i);
+        TEST_ASSERT_EQUAL(BTS71220_DCR_MUX_IS_SLEEP_Z, dcr_reg.mux);
+    }
 }
-
+ 
 int runUnityTests(void)
 {
     UNITY_BEGIN();
